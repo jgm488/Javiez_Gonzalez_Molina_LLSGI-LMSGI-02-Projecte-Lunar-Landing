c1c7c257d8b7667b355c1b40c38d860507f41e30
jgm488 <javier@pcpalma.es>
2016-12-22 22:58:31 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,85 @@
+var y = 10; // altura inicial y0=10%, debe leerse al iniciar si queremos que tenga alturas diferentes dependiendo del dispositivo
+var v = 0;
+var g = 1.622;
+var a = g;
+var dt = 0.016683;
+var timer=null;
+var timerFuel=null;
+var fuel=100;
+
+//al cargar por completo la página...
+window.onload = function(){
+	//definición de eventos
+	//mostrar menú móvil
+    	document.getElementById("showm").onclick = function () {
+		document.getElementsByClassName("c")[0].style.display = "block";
+		stop();
+	}
+	//ocultar menú móvil
+	document.getElementById("hidem").onclick = function () {
+		document.getElementsByClassName("c")[0].style.display = "none";
+		start();
+	}
+	//encender/apagar el motor al hacer click en la pantalla
+	document.onclick = function () {
+ 	  if (a==g){
+  		motorOn();
+ 	  } else {
+  		motorOff();
+ 	  }
+	}
+	//encender/apagar al apretar/soltar una tecla
+	document.onkeydown = motorOn;
+	document.onkeyup = motorOff;
+	
+	//Empezar a mover nave
+	start();
+}
+
+//Definición de funciones
+function start(){
+	timer=setInterval(function(){ moverNave(); }, dt*1000);
+}
+
+function stop(){
+	clearInterval(timer);
+}
+
+function moverNave(){
+	v +=a*dt;
+	document.getElementById("velocidad").innerHTML=v;
+	y +=v*dt;
+	document.getElementById("altura").innerHTML=y;
+	
+	//mover hasta que top sea un 70% de la pantalla
+	if (y<70){ 
+		document.getElementById("nave").style.top = y+"%"; 
+	} else { 
+		if (v>3)
+		{	
+			document.getElementById("nave1").src = "img/Nave espacial3.png";	
+		}
+		else
+		{
+		}
+		stop();
+	}
+}
+function motorOn(){
+	a=-g;
+	document.getElementById("nave1").src = "img/Nave espacial2.png";
+	
+	if (timerFuel==null)
+	timerFuel=setInterval(function(){ actualizarFuel(); }, 10);	
+}
+function motorOff(){
+	a=g;
+	document.getElementById("nave1").src="img/Nave espacial.png";
+	clearInterval(timerFuel);
+	timerFuel=null;
+}
+function actualizarFuel(){
+	//Aquí hay que cambiar el valor del marcador de Fuel...
+	fuel-=0.1;
+	document.getElementById("fuel").innerHTML=fuel;	
+}
